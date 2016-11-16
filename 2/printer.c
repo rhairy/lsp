2ff9920380e79c4c4d541357dfde5dc1ad404038
rhairy <randall.harrison91@gmail.com>
2016-11-15 20:42:39 -0800

    Use getopts and comments

--- 2/printer.c
+++ 2/printer.c
@@ -1,39 +1,61 @@
-/* printer - Read from a file and print to standard output using stdio. */
+// printer - Open a user provided file stream and print to stdout one character at a time.
 
 #include <errno.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[]) 
 {
-	char *usage = "printer <FILEPATH>";
+	FILE *instream = NULL;
+	char *filepath = NULL;
 
-	if (argc != 2) {
-		printf("%s\n", usage);
+	char *usage = "printer -f <FILEPATH>";
+
+	// Get command line arguments.
+	int opt;
+	while ( (opt = getopt(argc, argv, "f:")) != -1 ) {
+		switch (opt) {
+		case 'f':
+			filepath = optarg;
+			break;
+		case '?':
+			if (optopt == 'f') {
+				printf("%s\n", usage);
+				return -1;
+			} else {
+				printf("%s\n", usage);
+				return -1;
+			}
+			break;
+		default:
+			break;
+		}
+	}
+	
+	// Open the input file stream.
+	instream = fopen(filepath, "r");
+	if (instream == NULL) {
+		perror("fopen()");
 		return -1;
 	}
 	
-	FILE *infile;   
 
-	infile = fopen(argv[1], "r");
-	if (infile == NULL) {
-		perror("fopen():");
-		return -1;
-	}
-
+	// Read characters from instream and print to stdout.
 	int c;
-	while ( (c = fgetc(infile)) != EOF) {
+	while ( (c = fgetc(instream)) != EOF) {
 		fputc(c, stdout);
 	}
-	if (ferror(infile)) {
-		perror("");
-	}
 	
-	if (fclose(infile) == EOF) {
-		perror("fclose():");
-		return -1;
+	// Check for erros on the input stream.
+	if (ferror(instream)) {
+		printf("Error reading from input stream.\n");
 	}
 
-	return 0;	
+	// Determine return code.
+	if (fclose(instream) == EOF) {
+		perror("fclose()");
+		return -1;
+	} else {
+		return 0;
+	}
 }
